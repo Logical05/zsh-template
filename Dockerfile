067fa126afbdb366e07f1bd0cba00dffb33ced74
Logical05 <logical.code.01@gmail.com>
2024-12-22 15:24:58 +0700

    add other options

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,15 @@
 FROM ubuntu:22.04
 
-ENV TERM=xterm-256color
-ENV COLORTERM=truecolor
-ENV LC_ALL=C.UTF-8
-
 ARG USERNAME=user
 ARG USER_UID=1000
 ARG USER_GID=$USER_UID
+ARG TIMEZONE=Asia/Bangkok
 
-RUN /bin/sh -c echo 'Asia/Bangkok' > /etc/timezone && \
-    ln -s /usr/share/zoneinfo/Asia/Bangkok /etc/localtime && \
-    apt-get update && apt-get install -y --no-install-recommends sudo tzdata && \
-    rm -rf /var/lib/apt/lists/*
-
-RUN groupadd -g  $USER_GID $USERNAME && \
+RUN /bin/sh -c echo '$TIMEZONE' > /etc/timezone && \
+    ln -s /usr/share/zoneinfo/$TIMEZONE /etc/localtime && \ 
+    apt-get update && apt-get install -y sudo tzdata && \
+    rm -rf /var/lib/apt/lists/* && \
+    groupadd -g  $USER_GID $USERNAME && \
     useradd -m -u $USER_UID -g $USERNAME $USERNAME && \
     echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME && \
     chmod 0440 /etc/sudoers.d/$USERNAME
@@ -21,9 +17,11 @@ RUN groupadd -g  $USER_GID $USERNAME && \
 USER $USERNAME
 WORKDIR /home/$USERNAME
 
-COPY --chown=$USERNAME:$USERNAME asset /home/$USERNAME
+# If you want to create a docker image from the source, you need to uncomment COPY and comment ADD below.
+# COPY --chown=$USERNAME:$USERNAME asset /tmp/asset
+# COPY setup-zsh.sh /tmp
 
-COPY setup.sh /tmp
-RUN sudo chmod +x /tmp/setup.sh && /tmp/setup.sh
+ADD --chown=$USERNAME:$USERNAME https://github.com/Logical05/zsh-template.git /tmp
+RUN sudo chmod +x /tmp/setup-zsh.sh && /tmp/setup-zsh.sh
 
 ENTRYPOINT [ "/bin/zsh" ]
--- setup-zsh.sh
+++ setup-zsh.sh
@@ -5,19 +5,19 @@ set -e
 sudo apt-get update && sudo apt-get install -y software-properties-common && sudo rm -rf /var/lib/apt/lists/*
 sudo add-apt-repository ppa:neovim-ppa/stable
 
-sudo apt-get update && sudo apt-get install -y --no-install-recommends zsh wget git exa tmux neovim curl && sudo rm -rf /var/lib/apt/lists/*
+sudo apt-get update && sudo apt-get install -y zsh wget git exa tmux neovim curl && sudo rm -rf /var/lib/apt/lists/*
 
 sudo apt-get update && sudo apt-get upgrade -y && sudo rm -rf /var/lib/apt/lists/*
 sudo apt-get autoremove -y
 sudo apt-get clean -y
 
+mv /tmp/asset/.[!.]* ~
 
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-
 
 sed -i 's/ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 
@@ -28,6 +28,11 @@ zsh-autosuggestions \
 zsh-history-substring-search \
 z \
 )/' ~/.zshrc
+
+echo '\n
+export TERM=xterm-256color
+export COLORTERM=truecolor
+export LC_ALL=C.UTF-8' >> ~/.zshrc
 
 echo '\n
 alias vi=nvim
